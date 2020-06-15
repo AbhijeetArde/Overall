fffeab997ecd16a0e5699b45150a5fa5913b25f9
AbhijeetArde <41187312+AbhijeetArde@users.noreply.github.com>
2020-06-15 11:19:56 +0530

    Add files via upload

---
+++ web_cam_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Response
+import cv2
+ds_factor=0.6
+app = Flask(__name__)
+face_cascade=cv2.CascadeClassifier(r"C:\Users\abhijeet.p.arde\PycharmProjects\testing\haarcascade_frontalface_default.xml")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(video):
+    while True:
+        ret, frame = video.read()
+        frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor,
+                           interpolation=cv2.INTER_AREA)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in face_rects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            break
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame =  jpeg.tobytes()
+        # frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    video = cv2.VideoCapture(0)
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
