b3123ff3aafbc614b9ac36915836c0be44428863
AbhijeetArde <41187312+AbhijeetArde@users.noreply.github.com>
2020-06-16 17:15:55 +0530

    Add files via upload

---
+++ video_recorder.py
@@ -0,0 +1,79 @@
+import numpy as np
+import os
+import cv2
+
+
+filename = 'video.avi'
+frames_per_second = 24.0
+res = '720p'
+face_cascade=cv2.CascadeClassifier(r"C:\Users\abhijeet.p.arde\PycharmProjects\testing\haarcascade_frontalface_default.xml")
+SMILE_CASCADE = cv2.CascadeClassifier(r"C:\Users\abhijeet.p.arde\PycharmProjects\testing\haarcascade_smile.xml")
+
+# Set resolution for the video capture
+# Function adapted from https://kirr.co/0l6qmh
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+# Standard Video Dimensions Sizes
+STD_DIMENSIONS =  {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+
+# grab resolution dimensions and set video capture to it.
+def get_dims(cap, res='1080p'):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width,height = STD_DIMENSIONS[res]
+    ## change the current caputre device
+    ## to the resulting resolution
+    change_res(cap, width, height)
+    return width, height
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+      return  VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
+
+cap = cv2.VideoCapture(0)
+out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in face_rects:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        region_of_interest_bw = gray[y:y + h, x:x + w]
+        region_of_interest_color = frame[y:y + h, x:x + w]
+        smiles = SMILE_CASCADE.detectMultiScale(region_of_interest_bw, 1.7, 22)
+        for sx, sy, sw, sh in smiles:
+            print(sx, sy, sw, sh)
+            cv2.rectangle(region_of_interest_color, (sx, sy),
+                          (sx + sw, sy + sh), (0, 0, 255), 2)
+        break
+    ret, jpeg = cv2.imencode('.jpg', frame)
+    out.write(frame)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
