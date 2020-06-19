09b263fc6a7cced24eb9a23df96a13c607b1993a
AbhijeetArde <41187312+AbhijeetArde@users.noreply.github.com>
2020-06-19 13:12:07 +0530

    Update tom_brad_diff.py

--- tom_brad_diff.py
+++ tom_brad_diff.py
@@ -3,8 +3,8 @@ import numpy as np
 import os
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read(r'C:\Users\abhijeet.p.arde\PycharmProjects\testing\trainer.yml')
-cascadePath = r"C:\Users\abhijeet.p.arde\PycharmProjects\testing\haarcascade_frontalface_default.xml"
+recognizer.read(r'C:\Userstesting\trainer.yml')
+cascadePath = r"C:\Userstesting\haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
